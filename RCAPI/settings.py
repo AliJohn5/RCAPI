7746c3f6926a608fd1762258c07194c977d7c7d7
AliJohn5 <aliy65154@gmail.com>
2025-06-05 17:46:14 +0300

    fixig rooms

--- RCAPI/settings.py
+++ RCAPI/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = False
+DEBUG = True
 
 
 DEFENDER_LOGIN_FAILURE_LIMIT = 5  # Number of allowed login attempts
@@ -47,7 +47,9 @@ ASGI_APPLICATION = "RCAPI.asgi.application"
 CHANNEL_LAYERS = {
 }
 
-if(DEBUG):
+
+
+if(DEBUG == False):
     CHANNEL_LAYERS = {
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -57,6 +59,7 @@ if(DEBUG):
         },
     }
 else:
+    os.environ.setdefault("REDIS_URL","redis://red-d0teb2m3jp1c73egk34g:6379")
     CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -164,7 +167,7 @@ WSGI_APPLICATION = 'RCAPI.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {}
 
-if(DEBUG):
+if(DEBUG == False):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
